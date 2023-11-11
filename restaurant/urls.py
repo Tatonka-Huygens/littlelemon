6739412221fd3ef14c9c380e193cefab6e9c67c6
Luis Manyari <lmanyari@hotmail.com>
2023-11-11 15:58:33 -0500

    commit 5

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,24 @@
 #define URL route for index() view
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+'''
+router1 = DefaultRouter(trailing_slash=False)
+router1.register(r'users', views.UserViewSet, basename='users')
+'''
+   #path('restaurant/booking/', include(router.urls)),
+   #path('', include(router1.urls)),
+   #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuItemSerializer, UserSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+    
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+  
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+    
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated] 
