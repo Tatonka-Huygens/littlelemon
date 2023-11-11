9260bd55a0f1d1576734dc5421896af7a5233271
Luis Manyari <lmanyari@hotmail.com>
2023-11-11 18:14:06 -0500

    commit 5

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,10 +17,23 @@ Including another URLconf
 #update URLConf by including URL patterns of restaurant app
 from django.contrib import admin
 from django.urls import path, include
+from restaurant import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+'''
+router1 = DefaultRouter(trailing_slash=False)
+router1.register(r'users', views.UserViewSet, basename='users')
+'''
+   #
+   #path('', include(router1.urls)),
+   #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('restaurant/', include('restaurant.urls')),
    path('restaurant/menu/',include('restaurant.urls')),
+   path('restaurant/booking/', include(router.urls)),
 ]
