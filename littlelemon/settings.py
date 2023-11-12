d9bc77d9b4b0af8aeb40ee1cb39ec64cfb3d3fa6
Luis Manyari <lmanyari@hotmail.com>
2023-11-11 22:34:52 -0500

    commit 7

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'restaurant',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+),
+}
+
+DJOSER = {
+    "USER_ID_FIELD":"username"
+}
