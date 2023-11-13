4f08c23a95a6ef7f54f1b36245ba5046969c7dd9
Luis Manyari <lmanyari@hotmail.com>
2023-11-12 23:52:38 -0500

    commit 9

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,7 @@
+http://127.0.0.1:8000/restaurant/
 http://127.0.0.1:8000/auth/users/
 http://127.0.0.1:8000/auth/token/login/
 http://127.0.0.1:8000/restaurant/booking/tables/
+http://127.0.0.1:8000/restaurant/booking/tables/2/
+http://127.0.0.1:8000/restaurant/menu-items/
+http://127.0.0.1:8000/restaurant/menu-items/5/
--- restaurant/views.py
+++ restaurant/views.py
@@ -22,21 +22,21 @@ def msg(request):
 
 class BookingViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
-    #authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
     
 class MenuItemsView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-    #authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
     
   
 class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
     permission_classes = [IsAuthenticated]
-    #authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
 
