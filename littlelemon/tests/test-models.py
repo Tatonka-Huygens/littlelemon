754d04f5820a60282c26d2e6dbd696c611e838e5
Luis Manyari <lmanyari@hotmail.com>
2023-11-12 23:20:39 -0500

    commit 9

--- littlelemon/tests/test-models.py
+++ littlelemon/tests/test-models.py
@@ -1,13 +1,7 @@
 from django.test import TestCase
 from restaurant.models import Menu
 
-
-#TestCase class
-class MenuModelTest(TestCase):
+class MenuItemTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        return self.assertNotEqual(item, "IceCream : 80")
-        
-        
-        
-        
+        self.assertEqual(item, "IceCream : 80")
